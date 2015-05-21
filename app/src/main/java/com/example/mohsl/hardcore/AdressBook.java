8cb0b518d58ac424699d764745fcb217cee6ea13
Lukas Mohs <lukasmohs@gmail.com>
2015-05-21 08:03:31 +0200

    smaller fixes

---
+++ app/src/main/java/com/example/mohsl/hardcore/AdressBook.java
@@ -0,0 +1,113 @@
+package com.example.mohsl.hardcore;
+
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mohsl on 13.05.2015.
+ */
+public class AdressBook {
+
+    private static AdressBook instance;
+    private static DataSource dataSource;
+    private static KeyHandler keyHandler;
+    private  String USERNAME;
+    private  int USERID = 0;
+
+    private List<Contact> contactList;
+
+    public static  AdressBook getInstance(){
+        if(instance == null){
+            instance = new AdressBook();
+        }
+        return instance;
+    }
+
+    public AdressBook(){;
+        dataSource =  DataSource.getInstance(MainActivity.getAppContext());
+        contactList = dataSource.getAllContactsFromDb();
+        keyHandler = KeyHandler.getInstance();
+    }
+
+    public  String getUserName() {
+        return USERNAME ;
+    }
+    public  int  getUserId() {
+        return USERID;
+    }
+    public  void setUserName(String mUserName) {
+        USERNAME = mUserName ;
+    }
+
+    public int getContactId(String name) {
+        int id=0;
+        for (int i=0; i<contactList.size();i++){
+            if(contactList.get(i).getName().equals(name)){
+                id = contactList.get(i).getId();
+            }
+        }
+        return id;
+    }
+
+    public void storeNewContact(String name, Key publicKey){
+        Contact newContact = new Contact(getNumberOfContacts(), name, false, publicKey);
+        dataSource.storeContactInDb(newContact);
+        contactList = dataSource.getAllContactsFromDb();
+    }
+
+    public int getNumberOfContacts(){
+        return contactList.size();
+    }
+
+    public void storeOwnContact(String name){
+        Contact newContact = new Contact(getNumberOfContacts(), name, false, keyHandler.getPubKey());
+        dataSource.storeContactInDb(newContact);
+        setUserName(name);
+        contactList = dataSource.getAllContactsFromDb();
+    }
+
+    public List<String> getAllContactNames(){
+        List<String> contactNameList = new ArrayList<>();
+        for (int i=0; i<contactList.size();i++){
+            contactNameList.add(contactList.get(i).getName());
+        }
+        return contactNameList;
+    }
+
+    public boolean isContact(String name) {
+        boolean isFriend = false;
+        for (int i = 0; i < contactList.size(); i++) {
+            if (contactList.get(i).getName().equals(name)) {
+                isFriend = true;
+            }
+        }
+        return isFriend;
+    }
+
+    public Contact getContact(String name) {
+        Contact specificContact = null;
+        for (int i = 0; i < contactList.size(); i++) {
+            if (contactList.get(i).getName().equals(name)) {
+                specificContact = contactList.get(i);
+            }
+        }
+        return specificContact;
+    }
+
+    public List<Contact> getContactList(){
+        return contactList;
+    }
+
+    public void setReadMessage(String contactName){
+        dataSource.storeReadMessageInDb(contactName);
+        getContact(contactName).setUnreadMessageAvailable(false);
+    }
+
+    public void setUnreadMessage(String contactName){
+        dataSource.storeUnreadMessageInDb(contactName);
+        getContact(contactName).setUnreadMessageAvailable(true);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/mohsl/hardcore/DataSource.java
@@ -0,0 +1,166 @@
+package com.example.mohsl.hardcore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DataSource {
+
+    private static KeyHandler keyHandeler;
+    private static DataSource instance;
+    // Database fields
+    private SQLiteDatabase database;
+    private MySQLiteHelper dbHelper;
+    private String[] allColumns = {MySQLiteHelper.COLUMN_CONTACT_ID,
+            MySQLiteHelper.COLUMN_CONTACT, MySQLiteHelper.COLUMN_PUBLIC_KEY, MySQLiteHelper.COLUMN_UNREAD_MESSAGE};
+
+    public static DataSource getInstance(Context context)
+    {
+        if(instance == null) {
+            if(context == null)
+            {
+                throw new NullPointerException("context cannot be null for the first initialization");
+            }
+            instance = new DataSource(context);
+            keyHandeler = KeyHandler.getInstance();
+        }
+        return instance;
+    }
+
+    private DataSource(Context context) {
+        dbHelper = new MySQLiteHelper(context);
+        database = dbHelper.getWritableDatabase();
+        dbHelper.onUpgrade(database, 1, 1);
+    }
+
+
+    public List<Contact> getAllContactsFromDb() {
+        List<Contact> contacts = new ArrayList<Contact>();
+
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_CONTACTS,
+                new String[]{MySQLiteHelper.COLUMN_CONTACT_ID, MySQLiteHelper.COLUMN_CONTACT, MySQLiteHelper.COLUMN_PUBLIC_KEY, MySQLiteHelper.COLUMN_UNREAD_MESSAGE} , null, null, null, null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            //0 --> ID
+            //1 --> Name
+            //2 --> PUbKey
+            //3 --> MessageAvailable
+            contacts.add(new Contact(Integer.parseInt(cursor.getString(0)),cursor.getString(1), Boolean.parseBoolean(cursor.getString(3)), keyHandeler.getKeyFromSerialization(cursor.getString(2))));
+            cursor.moveToNext();
+        }
+        // make sure to close the cursor
+        cursor.close();
+        return contacts;
+    }
+
+    public Contact getContactFromDb(int contactId) {
+        Contact contact = null;
+
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_CONTACTS,
+                new String[]{MySQLiteHelper.COLUMN_CONTACT_ID, MySQLiteHelper.COLUMN_CONTACT, MySQLiteHelper.COLUMN_PUBLIC_KEY, MySQLiteHelper.COLUMN_UNREAD_MESSAGE} , MySQLiteHelper.COLUMN_CONTACT_ID + " = " + contactId, null, null, null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            //0 --> ID
+            //1 --> Name
+            //2 --> PUbKey
+            //3 --> MessageAvailable
+            contact = new Contact(Integer.parseInt(cursor.getString(0)),cursor.getString(1), Boolean.parseBoolean(cursor.getString(3)), keyHandeler.getKeyFromSerialization(cursor.getString(2)));
+            cursor.moveToNext();
+        }
+        // make sure to close the cursor
+        cursor.close();
+        return contact;
+    }
+
+    public List<Message> getConversationHistoryFromDb(int contactId) {
+        List<Message> messages = new ArrayList<Message>();
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_MESSAGES,
+                new String[]{MySQLiteHelper.COLUMN_MESSAGE_ID, MySQLiteHelper.COLUMN_SENDER_ID,
+                            MySQLiteHelper.COLUMN_RECEIVER_ID, MySQLiteHelper.COLUMN_MESSAGE},
+                            MySQLiteHelper.COLUMN_SENDER_ID + " = " + contactId +
+                            " OR " +  MySQLiteHelper.COLUMN_RECEIVER_ID + " = " + contactId ,
+                            null, null, null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            messages.add(new Message(Integer.parseInt(cursor.getString(1)),
+                                        Integer.parseInt(cursor.getString(2)), cursor.getString(3)));
+            cursor.moveToNext();
+        }
+        // make sure to close the cursor
+        cursor.close();
+        return messages;
+    }
+
+    public void storeMessageInDb(Message message)
+    {
+        final String Insert_Data="INSERT INTO " + MySQLiteHelper.TABLE_MESSAGES +
+                " VALUES( null, " + message.getSenderId() + ", " + message.getReceiverId() + ", '" + message.getMessageText() + "' )";
+        database.execSQL(Insert_Data);
+    }
+
+    public void storeUnreadMessageInDb(String sendername){
+        final String Insert_Data="UPDATE " + MySQLiteHelper.TABLE_CONTACTS +
+                " SET " + MySQLiteHelper.COLUMN_UNREAD_MESSAGE +"='true' WHERE " +
+                MySQLiteHelper.COLUMN_CONTACT + "= '" + sendername +"'";
+        database.execSQL(Insert_Data);
+    }
+
+    public void storeReadMessageInDb(String sendername){
+        final String Insert_Data="UPDATE " + MySQLiteHelper.TABLE_CONTACTS +
+                " SET " + MySQLiteHelper.COLUMN_UNREAD_MESSAGE +"='false' WHERE " +
+                MySQLiteHelper.COLUMN_CONTACT + "= '" + sendername + "'";
+        database.execSQL(Insert_Data);
+    }
+
+    public void storeContactInDb(Contact contact){
+
+        final String Insert_Data="INSERT INTO " + MySQLiteHelper.TABLE_CONTACTS +
+                " (" + MySQLiteHelper.COLUMN_CONTACT + ", " + MySQLiteHelper.COLUMN_UNREAD_MESSAGE +
+                ", " + MySQLiteHelper.COLUMN_PUBLIC_KEY  + ") " +
+                " VALUES ('" + contact.getName() + "', 'false', '" + keyHandeler.getSerializationFromKey(contact.getPubKey()) + "')";
+        database.execSQL(Insert_Data);
+    }
+
+    public String getContactNameFromDb(int contactId)
+    {
+        String contactName = "unknown";
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_CONTACTS,
+                allColumns,  MySQLiteHelper.COLUMN_CONTACT_ID + " = " + contactId , null, null, null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            contactName =cursor.getString(1);
+            cursor.moveToNext();
+        }
+        // make sure to close the cursor
+        cursor.close();
+        return contactName;
+    }
+
+    public int getContactIdFromDb(String contactName)
+    {
+        int contactId = 0;
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_CONTACTS,
+                allColumns,  MySQLiteHelper.COLUMN_CONTACT + " = '" + contactName + "'", null, null, null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            contactId =Integer.parseInt(cursor.getString(0));
+            cursor.moveToNext();
+        }
+        // make sure to close the cursor
+        cursor.close();
+        return contactId;
+    }
+
+
+    public void close() {
+        dbHelper.close();
+    }
+}
